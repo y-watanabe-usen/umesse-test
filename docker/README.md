ae217bc970965b0282086e2e01cd61c38e0bdf11
JunSakamoto <jun.sakamoto0@gmail.com>
2020-06-17 12:37:11 +0900

    docker refactor

--- docker/README.md
+++ docker/README.md
@@ -5,18 +5,10 @@ docker-compose up
 
 ## lambda
 ```
-curl -d '{"Key":"broadcasting-end1.mp3"}' http://localhost:9001/2015-03-31/functions/sample/invocations
-curl -d '{"Key":"broadcasting-start1.mp3"}' http://localhost:9001/2015-03-31/functions/sample/invocations
+curl -d '{"Bucket":"[bucket name]", "Key":"[file name]"}' http://localhost:9001/2015-03-31/functions/index/invocations
 ```
 
 ## s3
-
-### endpoint
 ```
-http://localhost:4572
-```
-
-### dashboard
-```
-http://localhost:8080
+http://localhost:9000
 ```
--- docker/lambda/index.js
+++ docker/lambda/index.js
@@ -1,22 +1,37 @@
 const aws = require('aws-sdk');
 const s3 = new aws.S3({
-  endpoint: 'http://localstack:4572',
-  s3ForcePathStyle: 'true',
+  endpoint: 'http://docker-s3:9000',
+  s3ForcePathStyle: 'true', // docker-lambda only
 });
 
 exports.handler = async (event, context, callback) => {
+  console.log(JSON.stringify(event));
+
   try {
+    if (!event.Bucket) throw 'Parameter is not a Bucket'
+    if (!event.Key) throw 'Parameter is not a Key'
+
     const params = {
-      Bucket: 'mybucket',
+      Bucket: event.Bucket,
       Key: event.Key,
     };
-    console.log(params);
     const ret = await s3.getObject(params).promise();
-    context.statusCode = 200;
-    return ret.Body.toString();
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: ret.Body.toString(),
+    };
   } catch (error) {
     console.log(error);
-    context.statusCode = 500;
-    return JSON.stringify(error);
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify(error)
+    };
   }
 };
