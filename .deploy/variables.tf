6192fe5c9732006219afb457af4ee3d4f4e6bc50
s-okita <s-okita@usen-next.jp>
2020-07-02 16:19:44 +0900

    deploy env

---
+++ .deploy/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {
+  type    = string
+  default = "ap-northeast-1"
+}
+
+variable "aws_access_key" {
+  type    = string
+default = "local"
+}
+variable "aws_secret_key" {
+  type    = string
+default = "local"
+}
+variable "aws_endpoint_url" {
+  type    = string
+  default = "http://localhost:4566"
+}
+
