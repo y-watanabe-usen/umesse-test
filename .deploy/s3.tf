0af84ee4d83c00942e422a7333eb1ede33bde471
s-okita <s-okita@usen-next.jp>
2020-07-02 16:36:27 +0900

    rename bucket name

--- .deploy/s3.tf
+++ .deploy/s3.tf
@@ -1,10 +1,10 @@
-resource "aws_s3_bucket" "presetdata" {
-  bucket        = "presetdata"
+resource "aws_s3_bucket" "umesse-contents" {
+  bucket        = "umesse-contents"
   acl           = "public-read"
   force_destroy = true
 }
-resource "aws_s3_bucket" "userbucket" {
-  bucket        = "userbucket"
+resource "aws_s3_bucket" "umesse-users" {
+  bucket        = "umesse-users"
   acl           = "public-read"
   force_destroy = true
 }
