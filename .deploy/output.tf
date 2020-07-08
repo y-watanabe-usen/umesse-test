5e3aa9fd56eaaaeb69020f5df8ceaa9de3667749
s-okita <s-okita@usen-next.jp>
2020-07-08 17:23:43 +0900

    revert twa

--- .deploy/output.tf
+++
@@ -1,8 +0,0 @@
-
-output "local_url" {
-  value = "http://localhost:4567/restapis/${aws_api_gateway_deployment.umesse_v1.rest_api_id}/v1/_user_request_${aws_api_gateway_resource.umesse.path}"
-}
-output "aws_url" {
-  value = "${aws_api_gateway_deployment.umesse_v1.invoke_url}${aws_api_gateway_resource.umesse.path}"
-}
-
