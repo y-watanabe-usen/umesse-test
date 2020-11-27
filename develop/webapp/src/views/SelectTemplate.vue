0ee56e04a8362d3f8c34ecf66cc21b1a8ebe3efa
Mayuu Onishi <m-onishi@usen-next.jp>
2020-11-27 18:18:47 +0900

    音声合成画面の作成

---
+++ develop/webapp/src/views/SelectTemplate.vue
@@ -0,0 +1,410 @@
+<template>
+  <div class="bg-umesse pb-5">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <router-link class="navbar-brand text-white" :to="{ path: '/' }"
+          >&lt;戻る</router-link
+        >
+        <div
+          class="collapse navbar-collapse justify-content-center text-white h4"
+        >
+          テンプレート選択
+        </div>
+      </nav>
+      <div class="row">
+        <div class="col-2 bg-menu pl-1 pr-1 rounded-left">
+          <button
+            type="button"
+            class="btn btn-menu text-left text-white"
+            :class="[
+              menu.id == state.activeMenuId ? 'btn-light' : 'btn-link',
+              menu.id == state.activeMenuId ? 'text-dark' : 'text-white',
+              menu.id == 1 ? 'mt-2' : '',
+            ]"
+            v-for="menu in state.menus"
+            :key="menu.id"
+            @click="state.activeMenuId = menu.id"
+          >
+            {{ menu.title }}
+          </button>
+        </div>
+        <div class="col-10 bg-white rounded-right">
+          <div class="my-3">
+            <h6 class="border-bottom border-gray pb-2 mb-0">
+              <select class="form-control w-25">
+                <option v-for="sort in state.sorts" :key="sort">
+                  {{ sort }}
+                </option>
+              </select>
+            </h6>
+            <div
+              class="media text-muted pt-3"
+              v-for="narrationData in state.narrationDatas"
+              :key="narrationData.title"
+            >
+              <div
+                class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray pl-3"
+              >
+                <div
+                  class="d-flex justify-content-between align-items-center w-100"
+                >
+                  <strong class="text-dark h5 pt-2 pb-2">{{
+                    narrationData.title
+                  }}</strong>
+                  <div>
+                    <button
+                      type="button"
+                      class="btn btn-light shadow btn-try mr-3"
+                      data-toggle="modal"
+                      data-target=".bd-try-modal-lg"
+                    >
+                      <svg
+                        width="1em"
+                        height="1em"
+                        viewBox="0 0 16 16"
+                        class="bi bi-play-fill"
+                        fill="currentColor"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"
+                        />
+                      </svg>
+                      試聴
+                    </button>
+                    <router-link :to="{ path: 'createnarrationtextinput' }">
+                    <button
+                      type="button"
+                      class="btn btn-light shadow btn-try"
+                    >
+                        選択
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="7.099"
+                          height="12.198"
+                          viewBox="0 0 7.099 12.198"
+                          class="ml-2"
+                        >
+                          <path
+                            d="M933.947,184.472l4.685,4.685-4.685,4.685"
+                            transform="translate(-932.533 -183.057)"
+                            fill="none"
+                            stroke="#578ed9"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                          />
+                        </svg>
+                    </button>
+                        </router-link>
+                  </div>
+                </div>
+                <span class="d-block pb-2"
+                  >{{ narrationData.description1 }}<br />{{
+                    narrationData.description2
+                  }}</span
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- modal -->
+    <div
+      class="modal fade bd-try-modal-lg"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="myLargeModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">試聴</h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="row">
+              <div class="col-4">
+                <template v-if="!state.isPlaying">
+                  <button
+                    type="button"
+                    class="btn btn-light shadow btn-play"
+                    @click="play"
+                  >
+                    <svg
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 16 16"
+                      class="bi bi-play-fill"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"
+                      />
+                    </svg>
+                    再生
+                  </button>
+                </template>
+                <template v-else>
+                  <button
+                    type="button"
+                    class="btn btn-light shadow btn-play"
+                    @click="stop"
+                  >
+                    <svg
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 16 16"
+                      class="bi bi-stop-fill"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M5 3.5h6A1.5 1.5 0 0 1 12.5 5v6a1.5 1.5 0 0 1-1.5 1.5H5A1.5 1.5 0 0 1 3.5 11V5A1.5 1.5 0 0 1 5 3.5z"
+                      />
+                    </svg>
+                    停止
+                  </button>
+                </template>
+              </div>
+              <div class="col-8">
+                <div class="row">
+                  <div class="col text-left" style="font-size: 17px">
+                    {{ state.playbackTimeHms }}
+                  </div>
+                  <div class="col text-right" style="font-size: 17px">
+                    {{ state.durationHms }}
+                  </div>
+                </div>
+                <meter
+                  min="0"
+                  :max="state.duration"
+                  class="w-100"
+                  :value="state.playbackTime"
+                ></meter>
+              </div>
+            </div>
+            <div class="row pt-5">
+              <div class="col-4">
+                タブレット音量<br />
+                <small>タブレットのスピーカーから音が出ます。</small>
+              </div>
+              <div class="col-8">
+                <div class="row">
+                  <div class="col text-left">
+                    <svg
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 16 16"
+                      class="bi bi-volume-down-fill"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M8.717 3.55A.5.5 0 0 1 9 4v8a.5.5 0 0 1-.812.39L5.825 10.5H3.5A.5.5 0 0 1 3 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06z"
+                      />
+                      <path
+                        d="M10.707 11.182A4.486 4.486 0 0 0 12.025 8a4.486 4.486 0 0 0-1.318-3.182L10 5.525A3.489 3.489 0 0 1 11.025 8c0 .966-.392 1.841-1.025 2.475l.707.707z"
+                      />
+                    </svg>
+                  </div>
+                  <div class="col text-center">volume 32</div>
+                  <div class="col text-right">
+                    <svg
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 16 16"
+                      class="bi bi-volume-up-fill"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"
+                      />
+                      <path
+                        d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"
+                      />
+                      <path
+                        d="M8.707 11.182A4.486 4.486 0 0 0 10.025 8a4.486 4.486 0 0 0-1.318-3.182L8 5.525A3.489 3.489 0 0 1 9.025 8 3.49 3.49 0 0 1 8 10.475l.707.707z"
+                      />
+                      <path
+                        fill-rule="evenodd"
+                        d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06z"
+                      />
+                    </svg>
+                  </div>
+                </div>
+                <meter min="0" max="15" class="w-100" value="1"></meter>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer text-center">
+            <button
+              type="button"
+              class="btn btn-light btn-close"
+              data-dismiss="modal"
+              @click="stop"
+            >
+              終了
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, reactive } from "vue";
+import AudioPlayer from "@/mixin/AudioPlayer";
+import axios from "axios";
+import AudioStore from "@/store/audio";
+
+export default {
+  setup() {
+    const audioPlayer = AudioPlayer();
+    const audioStore = AudioStore();
+    const state = reactive({
+      menus: [
+        {
+          id: 1,
+          title: "テンプレート1",
+        },
+        {
+          id: 2,
+          title: "テンプレート2",
+        },
+        {
+          id: 3,
+          title: "テンプレート3",
+        },
+        {
+          id: 4,
+          title: "テンプレート4",
+        },
+        {
+          id: 5,
+          title: "テンプレート5",
+        },
+        {
+          id: 6,
+          title: "テンプレート6",
+        },
+      ],
+      activeMenuId: 1,
+      sorts: ["名前順", "作成日順", "更新日順"],
+      narrationDatas: [
+        {
+          title: "XXXXXXの案内1",
+          description1:
+            "本日はご来店いただきまして、誠にありがとうございます。お客様に…",
+          description2: "00:24 放送開始日2020年10月15日 有効期限2020年10月20日",
+        },
+        {
+          title: "XXXXXXの案内1",
+          description1:
+            "お客様にご案内申し上げます。当店ではアルバイトを募集いたしており…",
+          description2: "00:15 放送開始日2020年10月15日 有効期限2020年10月20日",
+        },
+        {
+          title: "XXXXXXの案内1",
+          description1:
+            "本日はご来店いただきまして、誠にありがとうございます。お客様に…",
+          description2: "00:24 放送開始日2020年10月15日 有効期限2020年10月20日",
+        },
+        {
+          title: "XXXXXXの案内1",
+          description1:
+            "本日はご来店いただきまして、誠にありがとうございます。お客様に…",
+          description2: "00:24 放送開始日2020年10月15日 有効期限2020年10月20日",
+        },
+      ],
+      isPlaying: computed(() => audioPlayer.isPlaying()),
+      playbackTime: computed(() => {
+        return audioPlayer.getPlaybackTime();
+      }),
+      playbackTimeHms: computed(() => {
+        return sToHms(Math.floor(audioPlayer.getPlaybackTime()));
+      }),
+      duration: computed(() => {
+        return audioPlayer.getDuration();
+      }),
+      durationHms: computed(() => {
+        return sToHms(Math.floor(audioPlayer.getDuration()));
+      }),
+    });
+    // 秒を時分秒に変換
+    const sToHms = (second: number) => {
+      const h = "" + ((second / 36000) | 0) + ((second / 3600) % 10 | 0);
+      const m =
+        "" + (((second % 3600) / 600) | 0) + (((second % 3600) / 60) % 10 | 0);
+      const s = "" + (((second % 60) / 10) | 0) + ((second % 60) % 10);
+      return h + ":" + m + ":" + s;
+    };
+    const play = async () => {
+      if (state.isPlaying) return;
+      // TODO: umesse apiから取得
+      const signedUrl = "/audio/asahi/music/myuu/wave/hana.mp3";
+
+      await audioStore.download(signedUrl);
+      audioPlayer.start(<AudioBuffer>audioStore.audioBuffer);
+    };
+
+    const stop = () => {
+      if (state.isPlaying) audioPlayer.stop();
+    };
+
+    return {
+      state,
+      play,
+      stop,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.bg-menu {
+  background: -moz-linear-gradient(top, #89929e, #a4a8ad);
+  background: -webkit-linear-gradient(top, #89929e, #a4a8ad);
+  background: linear-gradient(to bottom, #89929e, #a4a8ad);
+}
+.btn-menu {
+  width: 100%;
+  height: 80px;
+  margin-bottom: 10px;
+  text-decoration: none;
+}
+.btn-try,
+.btn-edit {
+  width: 100px;
+  height: 40px;
+}
+.btn-edit {
+  margin-left: 20px;
+}
+.btn:focus {
+  box-shadow: none;
+}
+.btn-play,
+.btn-close {
+  width: 200px;
+}
+.btn-link {
+  color: #333;
+}
+.dropdown-toggle::after {
+  content: none;
+}
+</style>
