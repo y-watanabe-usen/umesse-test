1e23dde3b9ccd3fc55455a392c117de712fdc13f
s-okita <s-okita@usen-next.jp>
2020-12-23 15:27:24 +0900

    fix issue-191

--- develop/webapp/src/services/uploadRecordingService.ts
+++ develop/webapp/src/services/uploadRecordingService.ts
@@ -1,45 +1,52 @@
-import { reactive } from 'vue';
+import { reactive } from "vue";
 
 export interface RecordingFile {
-    title: string | undefined;
-    description: string | undefined;
-    blob: Blob | undefined;
+  title: string | undefined;
+  description: string | undefined;
+  blob: Blob | undefined;
 }
 
 export enum UPLOAD_RECORDING_STATE {
-    NONE,
-    UPLOADING,
-    UPLOADED,
-    ERROR,
+  NONE,
+  UPLOADING,
+  UPLOADED,
+  ERROR,
 }
 
 interface FunctionInterface {
-    createUserRecording(s: string, s2: string, s3: string): Promise<any>;
+  createUserRecording(s: string, s2: string, s3: string): Promise<any>;
 }
 export function useUploadRecordingService(api: FunctionInterface) {
-    const state = reactive({
-        status: UPLOAD_RECORDING_STATE.NONE as UPLOAD_RECORDING_STATE,
-    });
+  const state = reactive({
+    status: UPLOAD_RECORDING_STATE.NONE as UPLOAD_RECORDING_STATE,
+  });
 
-    const getStatus = () => (state.status);
+  const getStatus = () => state.status;
 
-    const upload = async (file: RecordingFile) => {
-        if (state.status === UPLOAD_RECORDING_STATE.UPLOADING) { throw new Error(`state is uploading`); }
-        // TODO: check arguments here.
+  const upload = async (file: RecordingFile) => {
+    if (state.status === UPLOAD_RECORDING_STATE.UPLOADING) {
+      throw new Error(`state is uploading`);
+    }
+    // TODO: check arguments here.
 
-        state.status = UPLOAD_RECORDING_STATE.UPLOADING;
+    state.status = UPLOAD_RECORDING_STATE.UPLOADING;
 
-        const url = URL.createObjectURL(file.blob);
-        api
-            .createUserRecording("sampleID", file.title!, url)
-            .then((value) => (state.status = UPLOAD_RECORDING_STATE.UPLOADED))
-            .catch((error) => (state.status = UPLOAD_RECORDING_STATE.ERROR))
-            .finally(() => URL.revokeObjectURL(url));
+    var fr = new FileReader();
+
+    fr.onload = function() {
+      //const url = URL.createObjectURL(file.blob);
+      api
+        .createUserRecording("sampleID", file.title!, fr.result as string)
+        .then((value) => (state.status = UPLOAD_RECORDING_STATE.UPLOADED))
+        .catch((error) => (state.status = UPLOAD_RECORDING_STATE.ERROR))
+//        .finally(() => URL.revokeObjectURL(url));
     };
-    const reset = () => (state.status = UPLOAD_RECORDING_STATE.NONE);
-    return {
-        upload,
-        reset,
-        getStatus,
-    };
+    fr.readAsBinaryString(file.blob!!);
+  };
+  const reset = () => (state.status = UPLOAD_RECORDING_STATE.NONE);
+  return {
+    upload,
+    reset,
+    getStatus,
+  };
 }
