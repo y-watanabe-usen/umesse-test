7f71c0722e2390dd8b5db8fbc699020940308863
竹本 雅彦 <takemoto@sosolu.me>
2021-01-30 03:22:21 +0900

    Add modal dialog component

---
+++ develop/webapp/src/components/molecules/ModalDialog.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="modal-dialog" @click="onClick">
+    <div class="base" :class="[size]">
+      <slot name="header" />
+      <slot name="contents" />
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { reactive, SetupContext } from "vue";
+
+export default {
+  name: "ModalDialog",
+  props: {
+    size: {
+      type: String,
+      default: "medium",
+    }
+  },
+  setup(props: any, context: SetupContext) {
+    const state = reactive({});
+    const onClick = (event: Event) => {
+      if (event.target === document.querySelector(".modal-dialog")) {
+        context.emit('close');
+      }
+    }
+    return {
+      state,
+      onClick,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/_variables.scss';
+
+.modal-dialog {
+  @include flex_center;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.3);
+  .base {
+    background-color: white;
+    border-radius: 10px;
+    box-shadow: $box_shadow_strong;
+    &.small {
+      width: 600px;
+    }
+    &.medium {
+      width: 900px;
+    }
+    &.large {
+      width: 1040px;
+    }
+  }
+}
+</style>
---
+++ develop/webapp/src/components/molecules/ModalHeader.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="modal-header">
+    <p class="title">{{ title }}</p>
+    <button class="btn-close" @click="$emit('close')"><img src="@/assets/icon_close.svg" /></button>
+  </div>
+</template>
+
+<script lang="ts">
+import { reactive } from "vue";
+
+export default {
+  name: "ModalHeader",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    }
+  },
+  setup() {
+    const state = reactive({});
+    return {
+      state,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/_variables.scss';
+
+.modal-header {
+  position: relative;
+  height: 80px;
+  border-bottom: 1px solid rgb(229, 229, 229);
+  .title {
+    color: black;
+    font-size: 16px;
+    font-weight: $font_weight_bold;
+    line-height: 80px;
+    text-align: center;
+  }
+  .btn-close {
+    cursor: pointer;
+    position: absolute;
+    top: 25px;
+    right: 25px;
+    img {
+      width: 30px;
+      height: 30px;
+    }
+  }
+}
+</style>
