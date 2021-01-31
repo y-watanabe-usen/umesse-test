827ff5b4e820af817beeadee05d0ad67bde9106b
竹本 雅彦 <takemoto@sosolu.me>
2021-01-31 20:02:37 +0900

    Add button component

---
+++ develop/webapp/src/components/atoms/Button.vue
@@ -0,0 +1,60 @@
+<template>
+  <button @click="$emit('click')" :class="[ type, isDisabled ? 'disabled' : '' ]"><slot /></button>
+</template>
+
+<script lang="ts">
+import { reactive } from "vue";
+
+export default {
+  name: "Header",
+  props: {
+    type: {
+      type: String,
+      default: "normal",
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup() {
+    const state = reactive({});
+    return {
+      state,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/_variables.scss';
+
+button {
+  display: inline-block;
+  height: 50px;
+  color: $color_blue;
+  font-size: 16px;
+  font-weight: $font_weight_bold;
+  line-height: 50px;
+  padding-left: 36px;
+  padding-right: 36px;
+  border-radius: 25px;
+  background-color: white;
+  box-shadow: $box_shadow_weak;
+  cursor: pointer;
+  &.primary {
+    font-size: 20px;
+    color: white;
+    background-color: $color_blue;
+  }
+  &.secondary {
+    font-size: 20px;
+    color: black;
+    background-color: white;
+  }
+  &.disabled {
+    color: white;
+    background-color: rgb(173, 173, 173);
+  }
+}
+</style>
