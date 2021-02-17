75f23bc41727155e34499ef07faa877fdcb67a96
s-okita <s-okita@usen-next.jp>
2021-02-17 10:04:49 +0900

    user api エラー処理

--- develop/lambda/api/umesse/user.js
+++ develop/lambda/api/umesse/user.js
@@ -1,8 +1,9 @@
 "use strict";
 
-const { constants, debuglog } = require("umesse-lib/constants");
+const { constants, debuglog, errorlog } = require("umesse-lib/constants");
 const { validation } = require("umesse-lib/validation");
 const { dynamodbManager } = require("umesse-lib/utils/dynamodbManager");
+const { BadRequestError, InternalServerError } = require("./error");
 
 // ユーザーデータ取得
 exports.getUser = async (unisCustomerCd) => {
@@ -12,41 +13,44 @@ exports.getUser = async (unisCustomerCd) => {
     })}`
   );
 
-  try {
-    // パラメーターチェック
-    const checkParams = validation.checkParams({
-      unisCustomerCd: unisCustomerCd,
-    });
-    if (checkParams) throw checkParams;
+  // パラメーターチェック
+  const checkParams = validation.checkParams({
+    unisCustomerCd: unisCustomerCd,
+  });
+  if (checkParams) throw new BadRequestError(checkParams);
 
-    const key = { unisCustomerCd: unisCustomerCd };
-    const options = {
-      ProjectionExpression:
-        "unisCustomerCd," +
-        "contractCd," +
-        "serviceCd," +
-        "serviceName," +
-        "customerName," +
-        "customerNameKana," +
-        "customerGroupCd," +
-        "customerGroupName," +
-        "contractStatusCd," +
-        "contractStatusName," +
-        "createDate," +
-        "renewalDate",
-    };
-    debuglog(JSON.stringify({ key: key, options: options }));
+  const user = await findUserByCd(unisCustomerCd);
 
-    const res = await dynamodbManager.get(
-      constants.dynamoDbTable().users,
-      key,
-      options
-    );
-    if (!res || !res.Item) throw "not found";
-    return res.Item;
-  } catch (e) {
-    // TODO: error handle
-    console.log(e);
-    return { message: e };
-  }
+  return user;
 };
+
+
+async function findUserByCd(unisCustomerCd) {
+  const key = { unisCustomerCd: unisCustomerCd };
+  const options = {
+    ProjectionExpression:
+      "unisCustomerCd," +
+      "contractCd," +
+      "serviceCd," +
+      "serviceName," +
+      "customerName," +
+      "customerNameKana," +
+      "customerGroupCd," +
+      "customerGroupName," +
+      "contractStatusCd," +
+      "contractStatusName," +
+      "createDate," +
+      "renewalDate",
+  };
+  debuglog(JSON.stringify({ key: key, options: options }));
+  let res;
+  try {
+    res = await dynamodbManager.get(constants.dynamoDbTable().users, key, options);
+  } catch (e) {
+    errorlog(JSON.stringify(e));
+    throw new InternalServerError(e.message);
+  }
+
+  if (!res || !res.Item) throw new InternalServerError("not found");
+  return res.Item;
+}
