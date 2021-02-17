caf649e0268f199df56d63b25cd0baca5f850f61
y-dobashi <y.dobashi@gmail.com>
2021-02-17 12:09:18 +0900

    revert

--- tools/local_win.sh
+++ tools/local_win.sh
@@ -9,25 +9,25 @@ LAMBDA_CONVERTER_DIR=$HOME_DIR/develop/lambda/converter
 UMESSE_LIB_DIR=$HOME_DIR/develop/lambda/layer/nodejs/node_modules/umesse-lib
 
 # start localstack
-#cd $LOCALSTACK_DIR
-#docker-compose down
-#docker system prune -af
-#docker-compose up -d --build
-#
-## start terraform
-#cd $LAMBDA_API_DIR
-#rm -rf tmp
-#npm pack ../layer/nodejs/node_modules/umesse-lib/ && npm install --no-save umesse-lib-0.0.1.tgz
-#
-#cd $LAMBDA_CONVERTER_DIR
-#npm pack ../layer/nodejs/node_modules/umesse-lib/ && npm install --no-save umesse-lib-0.0.1.tgz
-#
-#cd $DEPLOY_LOCAL_DIR
-#rm -rf .terraform* terraform.tfstate* umesse_*
-#terraform init
-#terraform plan
-#terraform apply -auto-approve 
-#sh ./init.sh
+cd $LOCALSTACK_DIR
+docker-compose down
+docker system prune -af
+docker-compose up -d --build
+
+# start terraform
+cd $LAMBDA_API_DIR
+rm -rf tmp
+npm pack ../layer/nodejs/node_modules/umesse-lib/ && npm install --no-save umesse-lib-0.0.1.tgz
+
+cd $LAMBDA_CONVERTER_DIR
+npm pack ../layer/nodejs/node_modules/umesse-lib/ && npm install --no-save umesse-lib-0.0.1.tgz
+
+cd $DEPLOY_LOCAL_DIR
+rm -rf .terraform* terraform.tfstate* umesse_*
+terraform init
+terraform plan
+terraform apply -auto-approve 
+sh ./init.sh
 
 cd $UMESSE_LIB_DIR
 npm i
