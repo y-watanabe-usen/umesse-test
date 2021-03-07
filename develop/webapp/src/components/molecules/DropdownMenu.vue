6ab157ff9541429087c357d2650683fb843b9558
竹本 雅彦 <takemoto@sosolu.me>
2021-03-07 21:50:03 +0900

    Implement dropdown menu.

--- develop/webapp/src/components/molecules/DropdownMenu.vue
+++ develop/webapp/src/components/molecules/DropdownMenu.vue
@@ -1,14 +1,17 @@
 <template>
-  <ul class="dropdown-menu" :style="state.style">
-    <li
-      v-for="param in params"
-      :key="param.title"
-      :class="{ caution: param.isCaution }"
-      @click.stop="param.action()"
-    >
-      {{ param.title }}
-    </li>
-  </ul>
+  <div class="dropdown-menu" :style="state.style">
+    <ul>
+      <li
+        v-for="param in params"
+        :key="param.title"
+        :class="{ caution: param.isCaution }"
+        @click.stop="param.action()"
+      >
+        {{ param.title }}
+      </li>
+    </ul>
+    <div class="arrow" :style="state.arrowStyle"></div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -51,11 +54,18 @@ export default {
       style: computed(() => {
         return {
           width: props.width + "px",
-          top: props.direction == "down" ? (props.targetHeight + 20) + "px" : "auto",
-          bottom: props.direction == "up" ? (props.targetHeight + 20) + "px" : "auto",
+          top: props.direction == "down" ? (props.targetHeight + 24) + "px" : "auto",
+          bottom: props.direction == "up" ? (props.targetHeight + 24) + "px" : "auto",
           left: (((props.targetWidth - props.width) / 2) + props.offset) + "px",
         };
-      })
+      }),
+      arrowStyle: computed(() => {
+        return {
+          top: props.direction == "down" ? "-12px" : "auto",
+          bottom: props.direction == "up" ? "-12px" : "auto",
+          left: (((props.width - 25) / 2) - props.offset) + "px",
+        };
+      }),
     });
     return {
       state,
@@ -70,30 +80,58 @@ export default {
 .dropdown-menu {
   display: block;
   position: absolute;
-  background-color: white;
-  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.5);
-  border-radius: 8px;
   padding: 0;
   margin: 0;
   border: 0;
   z-index: 100;
-  li {
-    font-size: 16px;
-    font-weight: $font_weight_bold;
-    line-height: 60px;
-    height: 60px;
-    border-top: 1px solid rgb(232, 232, 232);
-    margin-left: 10px;
-    margin-right: 10px;
-    text-align: center;
-    cursor: pointer;
-    &:first-child {
-      border-top: none;
+  background-color: transparent;
+  ul {
+    background-color: white;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.5);
+    border-radius: 8px;
+    margin-bottom: 0;
+    li {
+      font-size: 16px;
+      font-weight: $font_weight_bold;
+      line-height: 60px;
+      height: 60px;
+      border-top: 1px solid rgb(232, 232, 232);
+      margin-left: 10px;
+      margin-right: 10px;
+      text-align: center;
+      cursor: pointer;
+      position: relative;
+      z-index: 3;
+      &:first-child {
+        border-top: none;
+      }
+      &.caution {
+        color: rgb(155, 0, 0);
+        border-top: 1px solid rgb(141, 141, 141);
+      }
     }
-    &.caution {
-      color: rgb(155, 0, 0);
-      border-top: 1px solid rgb(141, 141, 141);
+    &::after {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      border-radius: 8px;
+      background-color: white;
+      z-index: 2;
     }
+  }
+  .arrow {
+    position: absolute;
+    width: 0;
+    height: 0;
+    width: 25px;
+    height: 25px;
+    background-color: white;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.5);
+    transform: rotate(45deg) skew(10deg, 10deg);
+    z-index: 1;
   }
 }
 </style>
