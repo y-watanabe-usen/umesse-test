d36cab1a380bbc6b4e9a38c7f9d05498adefef98
JunSakamoto <32352135+JunSakamoto@users.noreply.github.com>
2021-03-25 12:35:30 +0900

    update terraform aws

--- deploy/aws/variables.tf
+++ deploy/aws/variables.tf
@@ -7,6 +7,15 @@ variable "name" {
   ]
 }
 
+variable "domain" {
+  type = map(string)
+  default = {
+    dev-umesse ="dev.umesse.usen.com",
+    stg-umesse ="stg.umesse.usen.com",
+    umesse ="umesse.usen.com",
+  }
+}
+
 variable "mock" {
   type = list(string)
   default = [
