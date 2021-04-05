6df4b7a27523183482c492509a6f6bcd231ca1f0
Yoshitomo Watanabe <yosh-watanabe@usen-next.jp>
2021-04-05 11:46:52 +0900

    #451 アップロード部分の作りこみ ・アップロードの為のuploadServiceを作成。 ・管理画面からU MUSIC・Ssenceへアップロードした場合の成功・失敗のダイアログを表示する実装。

---
+++ develop/webapp/src/models/UploadCreateRequestItem.ts
@@ -0,0 +1,10 @@
+// To parse this data:
+//
+//   import { Convert, UpdateUserCMRequestItem } from "./file";
+//
+//   const updateUserCMRequestItem = Convert.toUpdateUserCMRequestItem(json);
+
+export interface UploadCreateRequestItem {
+  uploadSystem?: string;
+}
+
