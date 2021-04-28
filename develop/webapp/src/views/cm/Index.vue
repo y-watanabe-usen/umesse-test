9842e48b1b83a1b14b6b39c9cb0061d99cc53871
Yoshitomo Watanabe <yosh-watanabe@usen-next.jp>
2021-04-28 16:23:56 +0900

    #760 キャッシュの削除をhome.vueに遷移時に修正。

--- develop/webapp/src/views/cm/Index.vue
+++ develop/webapp/src/views/cm/Index.vue
@@ -909,7 +909,6 @@ export default defineComponent({
       closePlayModal();
     };
     const updateAndOpenSavedModal = async () => {
-      removeDisplayCache();
       saveAnalytics();
       try {
         openLoadingModal();
@@ -1171,14 +1170,6 @@ export default defineComponent({
       return aboutCmTime;
     };
 
-    const removeDisplayCache = () => {
-      displayCache.remove(DISPLAY_CACHE_KEY.VOICE_TEMPLATE_INDEX_INDUSTRY_CD);
-      displayCache.remove(DISPLAY_CACHE_KEY.VOICE_TEMPLATE_INDEX_SCENE_CD);
-      displayCache.remove(DISPLAY_CACHE_KEY.VOICE_TEMPLATE_INDEX_TEMPLATES);
-      displayCache.remove(DISPLAY_CACHE_KEY.VOICE_TEMPLATE_INDEX_SCENES);
-      displayCache.remove(DISPLAY_CACHE_KEY.VOICE_TEMPLATE_INDEX_SORT);
-    };
-
     return {
       ...toRefs(state),
       clearNarration,
@@ -1267,7 +1258,6 @@ export default defineComponent({
       backScreenName,
       toBackFunction,
       inputFocusBlur,
-      removeDisplayCache,
     };
   },
 });
