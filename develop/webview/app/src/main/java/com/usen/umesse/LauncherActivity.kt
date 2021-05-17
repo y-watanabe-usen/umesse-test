31197e095c005a49d32927a19bf3919954a2d5f4
JunSakamoto <32352135+JunSakamoto@users.noreply.github.com>
2021-05-17 12:20:57 +0900

    update

--- develop/webview/app/src/main/java/com/usen/umesse/LauncherActivity.kt
+++ develop/webview/app/src/main/java/com/usen/umesse/LauncherActivity.kt
@@ -1,19 +1,62 @@
 package com.usen.umesse
 
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Log
+import android.webkit.WebSettings
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class LauncherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate.")
         setContentView(R.layout.activity_main)
 
         val webView: WebView = findViewById(R.id.webView)
-        webView.loadUrl(R.string.launchUrl.toString())
+        if (BuildConfig.DEBUG) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+
+        webView.settings.also {
+            it.javaScriptEnabled = true
+            it.javaScriptCanOpenWindowsAutomatically = true
+            it.userAgentString =
+                "${it.userAgentString} ${BuildConfig.APPLICATION_ID} ${BuildConfig.VERSION_NAME}"
+            it.useWideViewPort = true
+            it.loadWithOverviewMode = true
+            it.textZoom = 100
+            it.cacheMode = WebSettings.LOAD_NO_CACHE
+        }
+
+        object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                Log.e(TAG, "onPageStarted.")
+                Log.e(TAG, url)
+                super.onPageStarted(view, url, favicon)
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                Log.e(TAG, "onPageFinished.")
+                Log.e(TAG, url)
+                super.onPageFinished(view, url)
+            }
+        }.also { webView.webViewClient = it }
+
+        webView.also {
+            it.clearHistory()
+            it.clearCache(true)
+        }
+        webView.loadUrl(BuildConfig.LAUNCH_URL + "?unisCustomerCd=${Application.localData!!.unisCustomerCd}")
     }
 
     override fun onDestroy() {
+        Log.d(TAG, "onDestroy.")
         super.onDestroy()
+    }
+
+    companion object {
+        private const val TAG = "LauncherActivity"
     }
 }
